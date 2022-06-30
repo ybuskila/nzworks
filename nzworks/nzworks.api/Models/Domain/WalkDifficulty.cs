7793dec2c03f8eb43cbbf22cc47c54280e141d89
buba <ybuskila@gmail.com>
2022-06-30 22:27:47 +0300

    End of Section 2

---
+++ nzworks/nzworks.api/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace nzworks.api.Models.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ nzworks/nzworks.api/data/NZWalkssDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using nzworks.api.Models.Domain;
+
+namespace nzworks.api.data
+{
+    public class NZWalkssDbContext : DbContext
+    {
+        public NZWalkssDbContext(DbContextOptions<NZWalkssDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+    }
+}
